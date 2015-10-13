a15abb5bcb89b765e824984e35996468bc53eabf
Sebastian Walker <sebasw9@vt.edu>
2015-10-13 14:58:38 +0000

    Updated static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,16 +4,18 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-  end
-
-  test "should get about" do
-    get :about
-    assert_response :success
+    assert_select "title", "Home | Listen to String Cheese"
   end
 
   test "should get contact" do
     get :contact
     assert_response :success
+    assert_select "title", "Contact | Listen to String Cheese"
   end
 
+  test "should get about" do
+    get :about
+    assert_response :success
+    assert_select "title", "About | Listen to String Cheese"
+  end
 end
